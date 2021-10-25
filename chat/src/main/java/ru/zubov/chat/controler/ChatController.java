e21e4f2334b859e1c27b91bbdf80c27ee51443c4
ZubovVP <Zubov.VP@yandex.ru>
2021-10-25 23:28:56 +0300

    Correct mistake.

--- chat/src/main/java/ru/zubov/chat/controler/ChatController.java
+++ chat/src/main/java/ru/zubov/chat/controler/ChatController.java
@@ -1,10 +1,12 @@
-package ru.zubov.ph.controler;
+package ru.zubov.chat.controler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import ru.zubov.ph.model.Msg;
+import ru.zubov.chat.model.Msg;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -17,13 +19,15 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * Version: $.
  * Date: 20.10.2021.
  */
+@PropertySource("classpath:application.properties")
 @Controller
+@Slf4j
 public class ChatController {
     private final List<Msg> messages = new CopyOnWriteArrayList<>();
 
     @MessageMapping("/chat/{id}/save")
     @SendTo("/chat/{id}/save")
-    public Msg save(@DestinationVariable int id, Msg message) throws Exception {
+    public Msg save(@DestinationVariable int id, Msg message) {
         messages.add(message);
         return message;
     }
--- chat/src/main/java/ru/zubov/chat/model/Msg.java
+++ chat/src/main/java/ru/zubov/chat/model/Msg.java
@@ -1,4 +1,4 @@
-package ru.zubov.ph.model;
+package ru.zubov.chat.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
